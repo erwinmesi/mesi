3dc62a1f13ad27c1615e2b42a8b592e10a838dfd
Erwin Mesias <mesiaserwin@gmail.com>
2018-12-30 01:51:21 +0800

    Nav Links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,18 +13,31 @@
           <small class="title">Full-Stack Web Developer</small>
         </div>
         <ul class="links row">
+          <li class="link-item upwork col-12">
+            <a class="hover-only" href="#">
+              About me
+            </a>
+            <span class="pipe">|</span>
+            <a class="hover-only" href="#">
+              Blog
+            </a>
+            <span class="pipe">|</span>
+            <a class="hover-only" href="#">
+              Projects
+            </a>
+          </li>
           <li class="link-item upwork col-md-4 offset-md-2">
-            <a class="hover-only" href="{{ MESI_UPWORK }}" target="_blank" rel="noopener">
+            <small class="caption">Hire me at Upwork</small>
+            <a class="hover-only contact" href="{{ MESI_UPWORK }}" target="_blank" rel="noopener">
               <img src="/images/upwork.png" alt="{{ APP_NAME }} Upwork account">
               upwork.com/fl/mesiaserwin
             </a>
-            <small class="caption">Hire me</small>
           </li>
           <li class="link-item mail col-md-4">
-            <a class="hover-only" href="mailto:{{ MESI_GMAIL }}" target="_blank" rel="noopener">
+            <small class="caption">Contact me at</small>
+            <a class="hover-only contact" href="mailto:{{ MESI_GMAIL }}" target="_blank" rel="noopener">
               <i class="fa fa-envelope mr-1"></i>{{ MESI_GMAIL }}
             </a>
-            <small class="caption">Contact me</small>
           </li>
         </ul>
       </div>
