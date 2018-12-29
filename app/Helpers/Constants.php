9aedce2c8a4d96cf073a16a2a2c06f0d62a596dc
Erwin Mesias <mesiaserwin@gmail.com>
2018-12-30 03:05:44 +0800

    mesiaserwin updated to mesi

--- app/Helpers/Constants.php
+++ app/Helpers/Constants.php
@@ -1,6 +1,6 @@
 <?php
 
-const APP_NAME = 'mesiaserwin';
+const APP_NAME = 'mesi';
 const MESI_EMAIL = 'mail@mesiaserwin.com';
 const MESI_GMAIL = 'mesiaserwin@gmail.com';
 const MESI_UPWORK = 'https://www.upwork.com/o/profiles/users/_~0199bd5921ffe7c943/';
