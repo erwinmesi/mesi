65394d512f6b1939c998b96bca48f09df8962488
Erwin Mesias <mesiaserwin@gmail.com>
2018-12-29 23:49:57 +0800

    Home Page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('style')
+  <link rel="stylesheet" href="/css/pages/home.css">
+@stop
+
+@section('body')
+  <div id="home-page">
+    <h1 class="app-name">{{ APP_NAME }}</h1>
+  </div>
+@stop
